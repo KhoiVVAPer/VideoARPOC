ec4db882818e50b43db43b97f8efd9c0e6242162
Anh Khoi <anhkhoi592014@gmail.com>
2022-04-06 12:45:34 +0700

    update dependency and add gitignore

--- android/app/src/main/java/com/videoarpoc/VideoPOCModule.java
+++ android/app/src/main/java/com/videoarpoc/VideoPOCModule.java
@@ -1,16 +1,21 @@
 package com.videoarpoc;
+import com.banuba.agora.plugin.model.ArEffect;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-
+import com.banuba.agora.plugin.BanubaEffectsLoader;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import java.util.List;
 
 public class VideoPOCModule extends ReactContextBaseJavaModule {
+    ReactApplicationContext ctx;
    VideoPOCModule(ReactApplicationContext context) {
        super(context);
+       ctx = context;
    }
 
    @NonNull
@@ -21,9 +26,11 @@ public class VideoPOCModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void joinChannel() {
-        ReactApplicationContext context = getReactApplicationContext();
-        Intent myIntent = new Intent(context, VideoPOCActivity.class);
+        List<ArEffect> effects = new BanubaEffectsLoader(ctx).loadEffects();
+        Log.d(null, "#42412312312312312" + effects.toString());
+        Toast.makeText(ctx, "effects: " + effects.toArray().length, Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(ctx, VideoPOCActivity.class);
         myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(myIntent);
+        ctx.startActivity(myIntent);
     }
 }
